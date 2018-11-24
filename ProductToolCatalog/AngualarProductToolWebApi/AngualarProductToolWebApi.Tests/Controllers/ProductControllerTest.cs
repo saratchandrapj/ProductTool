e21d467ddca72c45c8def8f7993c15709c5547ca
saratchandrapappuj <pjsc.50@gmail.com>
2018-11-24 23:38:34 +0530

    Adding unit test cases to API

--- ProductToolCatalog/AngualarProductToolWebApi/AngualarProductToolWebApi.Tests/Controllers/ProductControllerTest.cs
+++ ProductToolCatalog/AngualarProductToolWebApi/AngualarProductToolWebApi.Tests/Controllers/ProductControllerTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AngualarProductToolWebApi.Controllers;
+using DataAccess.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,79 @@ namespace AngualarProductToolWebApi.Tests.Controllers
 {
     class ProductControllerTest
     {
+
+        [TestMethod]
+        public void GetCategory()
+        {
+            bool isTrue = false;
+            ProductController P = new ProductController();
+            var result = P.GetCategory();
+
+            if(result.Result.Count>0)
+            {
+                isTrue = true;
+            }
+
+            Assert.IsTrue(isTrue);
+        }
+
+        [TestMethod]
+        public void AddProduct()
+        {
+            bool isTrue = false;
+            Product product = new Product();
+            product.Description = "testfrommUnittestcase";
+            product.Price = 10.00M;
+            product.ProductCategoryId = 1;
+            product.ProductName = "unitTestcaseProduct";
+
+            ProductController P = new ProductController();
+            var resp = P.AddProduct(product);
+
+            if(resp.Result > 0)
+            {
+                isTrue = true;
+            }
+            Assert.IsTrue(isTrue);
+        }
+
+        [TestMethod]
+        public void UpdateProduct()
+        {
+            bool isTrue = false;
+            Product product = new Product();
+            product.Description = "testfrommUnittestcase";
+            product.Price = 10.00M;
+            product.ProductCategoryId = 1;
+            product.ProductName = "unitTestcaseProduct";
+            product.ProductId = 12;
+
+            ProductController P = new ProductController();
+            var resp = P.UpdateProduct(product);
+
+            if(resp.Result > 0)
+            {
+                isTrue = true;
+            }
+
+            Assert.IsTrue(isTrue);
+        }
+
+
+        [TestMethod]
+        public void GetProducts()
+        {
+            bool isTrue = false;
+
+            ProductController P = new ProductController();
+            var resp = P.GetProducts();
+
+            if (resp.Result.Count() > 0)
+            {
+                isTrue = true;
+            }
+
+            Assert.IsTrue(true);
+        }
     }
 }
